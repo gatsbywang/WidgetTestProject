4226aa235c1b0d20fc3ee2b35c83a64a14e23b7d
花歹 <gatsbywang@126.com>
2017-10-14 21:41:03 +0800

    添加58同城loadingView（切换形状）

--- 58LoadingView/src/main/java/com/demo/widget/loadingviewfor58/MainActivity.java
+++ 58LoadingView/src/main/java/com/demo/widget/loadingviewfor58/MainActivity.java
@@ -1,13 +1,42 @@
 package com.demo.widget.loadingviewfor58;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ShapeView mShapeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShapeView = (ShapeView) findViewById(R.id.shape_view);
+    }
+
+    public void change(View view) {
+//        ValueAnimator valueAnimator = ObjectAnimator.ofInt(0, 4000);
+//        valueAnimator.setDuration(4000*1000);
+//        valueAnimator.setInterpolator(new LinearInterpolator());
+//        valueAnimator.start();
+//        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+//            @Override
+//            public void onAnimationUpdate(ValueAnimator animation) {
+//                Log.i("tag", String.valueOf(animation.getAnimatedValue()));
+//                mShapeView.exchange();
+//            }
+//        });
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                mShapeView.exchange();
+            }
+        }, 1000,1000);
     }
 }
--- 58LoadingView/src/main/java/com/demo/widget/loadingviewfor58/ShapeView.java
+++ 58LoadingView/src/main/java/com/demo/widget/loadingviewfor58/ShapeView.java
@@ -2,9 +2,13 @@ package com.demo.widget.loadingviewfor58;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.drawable.shapes.Shape;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
+
+import static com.demo.widget.loadingviewfor58.ShapeView.Shape.Circle;
 
 /**
  * Created by 花歹 on 2017/10/12.
@@ -14,32 +18,87 @@ import android.view.View;
  */
 
 public class ShapeView extends View {
-//    private Shape  mCurrentShape = C
+    private Shape mCurrentShape = Circle;
+    Paint mPaint;
+    private Path mPath;
 
     public ShapeView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public ShapeView(Context context, AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public ShapeView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        //画正方形
 
-        //画圆形
+        switch (mCurrentShape) {
+            case Square:
+                //画正方形
+                mPaint.setColor(Color.BLUE);
+                canvas.drawRect(0, 0, getWidth(), getHeight(), mPaint);
+                break;
 
-        //画三角形
+            case Circle:
+                //画圆形
+                int center = getWidth() / 2;
+                mPaint.setColor(Color.YELLOW);
+                canvas.drawCircle(center, center, center, mPaint);
+                break;
+
+            case Triangle:
+                //画等边三角形 Path 画路线
+                mPaint.setColor(Color.RED);
+                if (mPath == null) {
+                    //画路径
+                    mPath = new Path();
+                    mPath.moveTo(getWidth() / 2, 0);
+                    mPath.lineTo(0, (float) (getWidth()*Math.sin(Math.PI*60/180)));
+                    mPath.lineTo(getWidth(), (float) (getWidth()*Math.sin(Math.PI*60/180)));
+//                    mPath.lineTo(getWidth() / 2, 0);
+                    //路径闭环
+                    mPath.close();
+                }
+                canvas.drawPath(mPath, mPaint);
+                break;
+        }
+
     }
 
-    public enum shape{
-        Circle,Square,Triangle
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        setMeasuredDimension(Math.min(width, height), Math.min(width, height));
+    }
+
+    public void exchange() {
+        switch (mCurrentShape) {
+            case Circle:
+                mCurrentShape = Shape.Square;
+                break;
+            case Square:
+                mCurrentShape = Shape.Triangle;
+                break;
+
+            case Triangle:
+                mCurrentShape = Shape.Circle;
+                break;
+        }
+        postInvalidate();
+    }
+
+    public enum Shape {
+        Circle, Square, Triangle
 
     }
 
